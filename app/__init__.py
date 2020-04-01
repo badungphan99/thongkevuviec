974345fe4aa03d12a02cee0979cf0585a68e7c82
dungpb <badungphan99@gmail.com>
2020-04-01 21:10:18 +0700

    realease version 1.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 
-engine = db.create_engine('sqlite:///../Database/database.db', echo = False)
+engine = db.create_engine('sqlite:///Database/database.db', echo = False)
 Session = sessionmaker(bind=engine)
 session = Session()
